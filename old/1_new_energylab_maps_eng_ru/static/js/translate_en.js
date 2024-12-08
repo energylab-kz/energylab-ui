13f57f29db834bed8888e435cdc4c6672c369822
NoyanTM <doge.channel@gmail.com>
2024-12-08 15:57:21 +0500

    feat: combined version

--- old/1_new_energylab_maps_eng_ru/static/js/translate_en.js
+++
@@ -1,5 +0,0 @@
-$(document).on("click", "#lang_en", function(){
-  var url = window.location.pathname;
-  var filename = url.substring(url.lastIndexOf('/')+1);
-  window.open('../'+filename);
-});
--- old/1_new_energylab_maps_eng_ru/static/js/translate_ru.js
+++
@@ -1,5 +0,0 @@
-$(document).on("click", "#lang_ru", function(){
-  var url = window.location.pathname;
-  var filename = url.substring(url.lastIndexOf('/')+1);
-  window.open('./ru/'+filename);
-});
